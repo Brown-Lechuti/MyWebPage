09bacf78c7a1e198244027db2dd20f3d030ce54e
BrownLechuti <brownlechuti@gmail.com>
2021-07-01 17:11:13 +0200

    Adding function to change pic dynamically

--- script-main.js
+++ script-main.js
@@ -6,3 +6,11 @@ alert("Welcome!")
 function resetName(name) {
     name.innerHTML = "Brown";
 }
+function changePic(imgTag)
+{
+    imgTag.src = "https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/119450599_736550226896480_5868398601336072216_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=174925&_nc_eui2=AeEJWTpwfG-DjlozU3pWz7Qe0W1atdzJItLRbVq13Mki0g7KGPOQlnyyH7DdiERbkr1xjCerpTzNHr5Fwfb8r0-X&_nc_ohc=WUuuw3uPl2EAX8G5a_4&tn=Kf5aBWCEw3psxunu&_nc_ht=scontent-jnb1-1.xx&oh=a24162d9b2f7e95c3b28bc2e3a76330e&oe=60E3BFCE";
+}
+function resetPic(imgTag)
+{
+    imgTag.src = "https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/177839370_876499746234860_3171117178911982589_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeG3s17sme119RfPv6fiD_CYKuNBlrvJixsq40GWu8mLGz51yOSq2225mD1gFVzDRL0ojss8HXT0tVhGt1In8f_5&_nc_ohc=ectL6UCbsQ4AX-JWsr0&_nc_ht=scontent-jnb1-1.xx&oh=b727e7d1114006493c59bf4429a0e0b8&oe=60E231F5";
+}
