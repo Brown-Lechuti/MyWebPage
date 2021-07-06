dd0d0a9bc74120a017a0ad92b549e6d4017250ad
BrownLechuti <brownlechuti@gmail.com>
2021-07-06 20:57:05 +0200

    update media links

--- script-main.js
+++ script-main.js
@@ -8,9 +8,9 @@ function resetName(name) {
 }
 function changePic(imgTag)
 {
-    imgTag.src = "https://i.pinimg.com/564x/c3/3f/a3/c33fa36c52fc54a8ec748d091356184d.jpg";
+    imgTag.src = "https://brownlechuti.w3spaces.com/motivation.jpg?bypass-cache=1625169654";
 }
 function resetPic(imgTag)
 {
-    imgTag.src = "https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/177839370_876499746234860_3171117178911982589_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeG3s17sme119RfPv6fiD_CYKuNBlrvJixsq40GWu8mLGz51yOSq2225mD1gFVzDRL0ojss8HXT0tVhGt1In8f_5&_nc_ohc=ectL6UCbsQ4AX-JWsr0&_nc_ht=scontent-jnb1-1.xx&oh=b727e7d1114006493c59bf4429a0e0b8&oe=60E231F5";
+    imgTag.src = "https://brownlechuti.w3spaces.com/profile.png?bypass-cache=1625588081";
 }
