a9bf81955b374590611b6a0be46214f920c3ac9c
BrownLechuti <brownlechuti@gmail.com>
2021-07-07 14:21:09 +0200

    Change  vanilla JS to JQuery

--- script-main.js
+++ script-main.js
@@ -1,11 +1,31 @@
-alert("Welcome!")
 
-  function changeName(name) {
+ //JQuery funcion
+ $(document).ready(function(){
+        alert("Welcome");
+        $("#profile").hover(function(){
+            (this).src ="https://brownlechuti.w3spaces.com/motivation.jpg?bypass-cache=1625169654";
+        },
+        function(){
+            (this).src = "https://brownlechuti.w3spaces.com/profile.png?bypass-cache=1625588081";
+
+        });
+        $("#brown_word").hover(function(){
+            (this).innerHTML = "Botshelo";
+        },
+        function(){
+            (this).innerHTML = "Brown";
+        });
+        
+    });
+/*
+function changeName(name) {
     name.innerHTML= "Botshelo";
 }
 function resetName(name) {
     name.innerHTML = "Brown";
 }
+
+
 function changePic(imgTag)
 {
     imgTag.src = "https://brownlechuti.w3spaces.com/motivation.jpg?bypass-cache=1625169654";
@@ -13,4 +33,4 @@ function changePic(imgTag)
 function resetPic(imgTag)
 {
     imgTag.src = "https://brownlechuti.w3spaces.com/profile.png?bypass-cache=1625588081";
-}
+}*/
